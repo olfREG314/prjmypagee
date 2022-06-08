849c86ea96023e31b81f523c3cff8680b36eaf7b
olfreg314 <olfreg314@gmail.com>
2022-06-08 17:01:07 +0530

    added nav

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <NavBar />
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+export default {
+  name: "App",
+  components: { NavBar },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 0px;
+
+  /* ===GLOBAL VAR===== */
+}
+body {
+  margin: 0px;
+  padding: 0px;
+}
+
+:root {
+  --primaryGrey: #545454;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,81 @@
+<template>
+  <nav>
+    <div class="nav-link">
+      <ul>
+        <li v-for="nav in navList" :key="nav">
+          <button>
+            <a :href="nav.navLink">{{ nav.navName }}</a>
+          </button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navList: [
+        { navName: "profile", navLink: "#" },
+        { navName: "projects", navLink: "#" },
+        { navName: "notes", navLink: "#" },
+        { navName: "sayHi!", navLink: "#" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Fredericka+the+Great&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Fredericka+the+Great&family=Hubballi&display=swap");
+nav {
+  display: grid;
+  position: sticky;
+  width: 100vw;
+  height: 55px;
+  background-color: white;
+  border-bottom: 2px solid #000;
+  align-items: center;
+  justify-content: center;
+}
+.nav-link ul {
+  margin: 0px;
+  padding: 0px;
+}
+.nav-link ul li {
+  display: inline-block;
+  margin: 0px;
+  padding: 0px;
+}
+
+.nav-link ul li button {
+  /* font-family: 'Fredericka the Great', cursive; */
+  font-family: "Hubballi", cursive;
+  background-color: white;
+  padding: 5px;
+  border: none;
+  margin: 0px 18px;
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 23px;
+  /* identical to box height */
+  letter-spacing: 0.25em;
+  /* tag */
+  color: var(--primaryGrey);
+}
+.nav-link ul li button a:link {
+  color: var(--primary-grey);
+  text-decoration-line: none;
+  text-transform: uppercase;
+}
+.nav-link ul li button a:hover {
+  text-decoration: underline;
+}
+.nav-link ul li button a:active {
+  font-family: "Franklin Gothic Medium", sans-serif;
+}
+</style>

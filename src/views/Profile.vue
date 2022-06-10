7771ebec919b0b30fffb74bbd40a4a87f329cce3
olfreg314 <olfreg314@gmail.com>
2022-06-10 20:34:33 +0530

    added redacted script font and some update

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -22,8 +22,23 @@
       <div class="github-link">
         <i class="bi-github" role="img" aria-label="GitHub"></i>
       </div>
-      <div class="tag name-tag"></div>
-      <div class="about-me"></div>
+      <div class="tag name-tag">
+        <div class="border-cmpt">
+          <div class="name">olfREG314</div>
+        </div>
+      </div>
+      <div class="about-me">
+        <div class="border-cmpt">
+          <div class="about-me-text">
+            To pursue a career with a company having a global vision that
+            encourages creativity and offers an opportunity to learn and develop
+            both in professional and personal life . Problem solving is not only
+            my main skill but also my passion, and my passion for it makes me
+            focused for serving towards quality product and customer
+            satisfaction .
+          </div>
+        </div>
+      </div>
     </section>
     <section>
       <div class="tag skills-tag"></div>
@@ -47,9 +62,12 @@ export default {};
 .container {
   display: grid;
 }
-/* ===ICONS===== */
+.about-me-text {
+  font-family: redacted script;
+}
+/* ===ICONS CONFIG===== */
 .bi-github {
-  color: aquamarine;
-  font-size: 1em;
+  color: var(--lightGrey);
+  font-size: 61px;
 }
 </style>
